94d091a72dbb37f322ab5f62c663714f16f1394d
Balaji D <cb.en.u4cse18008@cb.students.amrita.edu>
2021-04-04 08:18:35 +0000

    Update Timer.js

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -43,7 +43,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("Oct 08, 2022 21:00:00").getTime();
+    this.deadline = new Date("May 15, 2021 21:00:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
