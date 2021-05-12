76567658578be11de4c793701c4747240f91dad0
Gana016 <jayam.ganapathi12@gmail.com>
2021-05-12 13:26:44 +0530

    image resize

--- src/components/main/Main3.js
+++ src/components/main/Main3.js
@@ -6,30 +6,37 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import { storage } from '../../fire'
 import Axios from 'axios'
 import fileDownload from 'js-file-download'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Main3 = ({ currentFile }) => {
   console.log(currentFile)
-  function download(){
+  function download() {
     var httpsReference = storage.refFromURL(currentFile.url)
     httpsReference
-    .getDownloadURL()
-    .then((url) => {
-    Axios.get(url, {
-    responseType: 'blob',
-    }).then((res) => {
-      fileDownload(res.data, currentFile.name)
-      })
+      .getDownloadURL()
+      .then((url) => {
+        Axios.get(url, {
+          responseType: 'blob',
+        }).then((res) => {
+          fileDownload(res.data, currentFile.name)
+        })
       })
       .catch((error) => {
-      console.log('ERROR')
-    })
+        console.log('ERROR')
+      })
   }
 
   return (
     <main>
+      <div className='car'>
+        <h2>
+          <Link onClick={download} style={{ alignItems: 'center' }}>
+            Click to download the Image!!!
+          </Link>
+        </h2>
+      </div>
+
       <div className='cards'>
-        <h2 ><Link onClick={download} style={{alignItems: 'center'}}>Click to download the Image!!!</Link></h2>
         <br></br>
         <Image src={currentFile.url} thumbnail />
       </div>
